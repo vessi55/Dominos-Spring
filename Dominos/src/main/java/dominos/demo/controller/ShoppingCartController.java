efe1acf337baae585fcb8f760d16ac3aa655b8a8
Vesela <vesikoceva1@gmail.com>
2019-03-05 17:12:16 +0200

    View shopping cart added

--- Dominos/src/main/java/dominos/demo/controller/ShoppingCartController.java
+++ Dominos/src/main/java/dominos/demo/controller/ShoppingCartController.java
@@ -1,6 +1,7 @@
 package dominos.demo.controller;
 
 import dominos.demo.model.DTOs.CommonResponseDTO;
+import dominos.demo.model.DTOs.ShoppingCartViewDto;
 import dominos.demo.model.daos.PizzaDao;
 import dominos.demo.model.daos.ShoppingCartDao;
 import dominos.demo.model.products.Pizza;
@@ -9,6 +10,7 @@ import dominos.demo.util.exceptions.EmptyShoppingCartException;
 import dominos.demo.util.exceptions.InvalidLogInException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +18,9 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpSession;
 import java.time.LocalDateTime;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class ShoppingCartController extends BaseController{
@@ -29,62 +34,36 @@ public class ShoppingCartController extends BaseController{
 
     @Autowired
     ShoppingCartDao shoppingCartDao;
-
-    @PostMapping(value = ("/pizzas/{id}/shoppingCart/add"))
-    public CommonResponseDTO addPizzaToShoppingCart(@PathVariable("id") long id, HttpSession session)  throws InvalidLogInException {
-        if(SessionManager.isLoggedIn(session)) { //TODO : exception for not logged in
+    @PostMapping(value = ("/pizzas/{id}/quantity/{quantity}/shoppingCart/add"))
+    public CommonResponseDTO addPizzaToShoppingCart(@PathVariable("id") long id, @PathVariable("quantity") int quantity,
+                                                    HttpSession session) throws InvalidLogInException {
+        if(SessionManager.isLoggedIn(session)) {
             HashMap<Pizza, Integer> shoppingCart;
             Pizza pizza = pizzaDao.getProductById(id);
             if (session.getAttribute(SHOPPING_CART) == null) {
                 session.setAttribute(SHOPPING_CART, new HashMap<Pizza, Integer>());
-                shoppingCart = (HashMap<Pizza, Integer>) session.getAttribute(SHOPPING_CART);
-                shoppingCart.put(pizza, 1);
-            } else {
-                shoppingCart = (HashMap<Pizza, Integer>) session.getAttribute("cart");
-                if (!shoppingCart.containsKey(pizza)) {
-                    shoppingCart.put(pizza, 1);
-                }
-                shoppingCart.put(pizza, shoppingCart.get(pizza) + 1);
+            }
+            shoppingCart = (HashMap<Pizza, Integer>) session.getAttribute(SHOPPING_CART);
+            if (!shoppingCart.containsKey(pizza)) {
+                shoppingCart.put(pizza, quantity);
+            }
+            else {
+                shoppingCart.put(pizza, shoppingCart.get(pizza) + quantity);
             }
             return new CommonResponseDTO("Pizza " + pizza.getName() + " is successfully added to your shopping cart!", LocalDateTime.now());
         }
         throw new InvalidLogInException("You are not logged in! Please log in to continue!");
     }
 
-
-    @PostMapping(value = ("/shoppingCart/order"))
-    public CommonResponseDTO makeOrder(HttpSession session) throws BaseException {
+    @GetMapping(value = "/myShoppingCart")
+    public List<ShoppingCartViewDto> getMyShoppingCart(HttpSession session) throws BaseException {
         if(SessionManager.isLoggedIn(session)) {
             if (session.getAttribute(SHOPPING_CART) != null) {
-                shoppingCartDao.makeOrder(session);
-                return new CommonResponseDTO("Your order was successful!", LocalDateTime.now());
+                return shoppingCartDao.viewShoppingCart(session);
+            } else {
+                throw new EmptyShoppingCartException("Your shopping cart is empty!");
             }
-            throw new EmptyShoppingCartException("Your shopping cart is empty!");
         }
-        throw new InvalidLogInException("You are not logged! Please log in to continue!");
+        throw new InvalidLogInException("Please log in to continue");
     }
-//    @GetMapping(value = ("/shoppingCart"))
-//    public ArrayList<ShoppingCartViewDto> viewCart(HttpSession session) throws BaseException{
-//        if(SessionManager.isLoggedIn(session)) {
-//            if (session.getAttribute(SHOPPING_CART) != null) {
-//                return productDao.viewShoppingCart(session);
-//            } else {
-//                throw new EmptyShoppingCartException("Your shopping cart is empty!");
-//            }
-//        }
-//        throw new InvalidLogInException("Please log in to continue");
-//    }    @PostMapping(value = ("/products/{id}/shoppingCart/add"))
-
-////    @GetMapping(value = ("/shoppingCart"))
-////    public ArrayList<ShoppingCartViewDto> viewCart(HttpSession session) throws BaseException{
-////        if(SessionManager.isLoggedIn(session)) {
-////            if (session.getAttribute(SHOPPING_CART) != null) {
-////                return productDao.viewShoppingCart(session);
-////            } else {
-////                throw new EmptyShoppingCartException("Your shopping cart is empty!");
-////            }
-////        }
-////        throw new InvalidLogInException("Please log in to continue");
-////    }
-
 }
--- Dominos/src/main/java/dominos/demo/model/products/Pizza.java
+++ Dominos/src/main/java/dominos/demo/model/products/Pizza.java
@@ -1,5 +1,7 @@
 package dominos.demo.model.products;
 import dominos.demo.model.enums.Size;
+
+import java.util.Objects;
 import java.util.Set;
 
 import lombok.Getter;
@@ -19,13 +21,30 @@ public class Pizza  {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
-    private double price;
-    private String description;
+    private double price;private String description;
     @Enumerated(EnumType.STRING)
     @Column(length = 20)
     private Size size;
     private double weight;
-    private int quantity;
     private String image_url;
 
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pizza pizza = (Pizza) o;
+        return id == pizza.id &&
+                Double.compare(pizza.price, price) == 0 &&
+                Double.compare(pizza.weight, weight) == 0 &&
+                Objects.equals(name, pizza.name) &&
+                Objects.equals(description, pizza.description) &&
+                size == pizza.size &&
+                Objects.equals(image_url, pizza.image_url);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, price, description, size, weight, image_url);
+    }
 }
